f8e9a82c03320d2d6b19b95d60fcf4b5e43f31ae
ZacharySeguin <zack142011@gmail.com>
2017-01-26 17:26:20 -0500

    Uploading project...

---
+++ app/src/main/java/edu/carleton/COMP2601/a1/Game.java
@@ -0,0 +1,9 @@
+package edu.carleton.COMP2601.a1;
+
+/**
+ * Created by Owner on 2017-01-26.
+ */
+
+public class Game {
+
+}
---
+++ app/src/main/java/edu/carleton/COMP2601/a1/MainActivity.java
@@ -0,0 +1,24 @@
+package edu.carleton.COMP2601.a1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button button10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
